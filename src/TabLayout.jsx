13e752519378bda29e3d9fbaa81b42e185c11847
b12705030 <tca940120@gmail.com>
2025-06-18 13:42:13 +0800

    更新說明

--- src/TabLayout.jsx
+++ src/TabLayout.jsx
@@ -1,22 +1,35 @@
-import { Outlet, NavLink } from 'react-router-dom'
+// src/TabLayout.jsx
+import { NavLink, Outlet } from 'react-router-dom'
+import { FaHome, FaCalendarAlt, FaFlask, FaBook, FaGavel } from 'react-icons/fa'
 import './styles/TabLayout.css'
 
-function TabLayout({ session, onLogout }) {
+function TabLayout() {
   return (
     <div className="tab-container">
       <div className="main-content">
         <Outlet />
       </div>
-
       <nav className="tab-bar">
-        <NavLink to="/" end>主畫面</NavLink>
-        <NavLink to="/calendar">行事曆</NavLink>
-        <NavLink to="/test">試做專區</NavLink>
-        <NavLink to="/blog">部落格</NavLink>
-        <NavLink to="/rules">規則</NavLink>
-        {session && (
-          <button onClick={onLogout}>登出</button>
-        )}
+        <NavLink to="/" end className={({ isActive }) => isActive ? 'active' : ''}>
+          <FaHome size={20} />
+          <span>首頁</span>
+        </NavLink>
+        <NavLink to="/calendar" className={({ isActive }) => isActive ? 'active' : ''}>
+          <FaCalendarAlt size={20} />
+          <span>行事曆</span>
+        </NavLink>
+        <NavLink to="/test" className={({ isActive }) => isActive ? 'active' : ''}>
+          <FaFlask size={20} />
+          <span>試做</span>
+        </NavLink>
+        <NavLink to="/blog" className={({ isActive }) => isActive ? 'active' : ''}>
+          <FaBook size={20} />
+          <span>部落格</span>
+        </NavLink>
+        <NavLink to="/rules" className={({ isActive }) => isActive ? 'active' : ''}>
+          <FaGavel size={20} />
+          <span>規則</span>
+        </NavLink>
       </nav>
     </div>
   )
